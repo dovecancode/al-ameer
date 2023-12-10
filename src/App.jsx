de7aef570ef4593daeffddf4bb8d9bde9697133c
dove <codegeekdove@gmail.com>
2023-12-10 23:12:12 +0800

    al-ameer new beginning

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Typography } from '@mui/material'
+
+function App() {
+  return (
+    <>
+      <Typography variant="body2" align="justify">
+        Al-Ameer
+      </Typography>
+    </>
+  )
+}
+
+export default App
