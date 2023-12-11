b5288395edca7192770fc483be0b700431100b89
IanPrinceD <princedumanhug@gmail.com>
2023-12-11 18:59:38 +0800

    #19 added code in index.css and file Data.jsx. finished the Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import AcUnitIcon from '@mui/icons-material/AcUnit'
 import { Typography } from '@mui/material'
 import { FaHome } from 'react-icons/fa'
+import Header from './components/Header/index'
 
 function App() {
   return (
     <>
       <AcUnitIcon />
       <FaHome />
+      <Header />
       <Typography variant="h1">Al-Ameer</Typography>
     </>
   )
