b8c542c9292b56eb8dc30e5e7085033a0b9c205d
dove <codegeekdove@gmail.com>
2023-12-11 11:42:09 +0800

    al-ameer new beginning v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import AcUnitIcon from '@mui/icons-material/AcUnit'
 import { Typography } from '@mui/material'
+import { FaHome } from 'react-icons/fa'
 
 function App() {
   return (
     <>
-      <Typography variant="body2" align="justify">
-        Al-Ameer
-      </Typography>
+      <AcUnitIcon />
+      <FaHome />
+      <Typography variant="h1">Al-Ameer</Typography>
     </>
   )
 }
