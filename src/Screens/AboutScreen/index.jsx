3553936d7adfca48f7b97bc6c60e39812d6e0917
IanPrinceD <princedumanhug@gmail.com>
2024-01-03 21:04:38 +0800

    #41 : created aboutuswelcome component and added image

--- src/Screens/AboutScreen/index.jsx
+++ src/Screens/AboutScreen/index.jsx
@@ -1,9 +1,11 @@
 import TitlePage from '../../components/TitlePage'
+import AboutUsWelcome from '../../components/AboutUsWelcome';
 
 function AboutScreen() {
   return (
     <div>
       <TitlePage pageTitle='About Us' bgimage='subhero-aboutus.jpg'/>
+      <AboutUsWelcome/>
     </div>
   );
 }
