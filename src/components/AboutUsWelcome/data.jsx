b419a6a38f21112a6461a75b7af5f160def1bc83
IanPrinceD <princedumanhug@gmail.com>
2024-01-04 22:13:11 +0800

    #41 : created data.jsx file and modified orientation of aboutuswelcome component to adopt in various screen size

---
+++ src/components/AboutUsWelcome/data.jsx
@@ -0,0 +1,8 @@
+export const items = [
+  'A Passion for Healing',
+  'All our best',
+  'Always Caring',
+  '5-Star Care',
+  'Believe in Us',
+  'A Legacy of Excellence',
+];
--- src/components/AboutUsWelcome/style.js
+++ src/components/AboutUsWelcome/style.js
@@ -5,3 +5,12 @@ export const WelcomeAboutUsSection = styled(Container)({
  paddingTop: '9rem',
  paddingBottom: '6rem'
 })
+
+export const CircleWithText = styled('span')({
+  display: 'inline-block',
+  backgroundColor: '#159EEC',
+  borderRadius: '50%',
+  width: '1.5rem',
+  height: '1.5rem',
+  marginRight: '1rem',
+});
