4c0cc3ba48b988198a74ad17e63678aab1eec8a9
dove <codegeekdove@gmail.com>
2024-01-06 10:47:35 +0800

    partially coded the services action options

---
+++ src/components/SevicesOptionContent/index.jsx
@@ -0,0 +1,7 @@
+import { OptionContent } from './style'
+
+function SevicesOptionContent() {
+  return <OptionContent>SevicesOptionContent</OptionContent>
+}
+
+export default SevicesOptionContent
---
+++ src/components/SevicesOptionContent/style.js
@@ -0,0 +1,7 @@
+import { Box } from '@mui/material'
+import { styled } from '@mui/system'
+
+export const OptionContent = styled(Box)(({ theme }) => ({
+  flex: 5,
+  background: 'steelblue',
+}))
