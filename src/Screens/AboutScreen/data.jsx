d7f0714a7dfab34f3bca527d43659a233920874c
IanPrinceD <princedumanhug@gmail.com>
2024-01-08 16:23:32 +0800

    #49 : finished creating the doctor card silder

---
+++ src/Screens/AboutScreen/data.jsx
@@ -0,0 +1,99 @@
+import doctor1 from '../../assets/img/doctorImg/doctor1.jpg';
+import doctor2 from '../../assets/img/doctorImg/doctor2.jpg';
+import doctor3 from '../../assets/img/doctorImg/doctor3.jpg';
+
+export const doctorInfo = [
+  {
+    doctorGroup: [
+      {
+        id: 1,
+        image: doctor1,
+        name: 'Dr. Olivia Richards',
+        specialist: 'Cardiologist',
+        linkedinLink: '',
+        fbLink: '',
+        instaLink: '',
+      },
+      {
+        id: 2,
+        image: doctor2,
+        name: 'Dr. Jonathan Miller',
+        specialist: 'Neurosurgeon',
+        linkedinLink: '',
+        fbLink: '',
+        instaLink: '',
+      },
+      {
+        id: 3,
+        image: doctor3,
+        name: 'Dr. Sophia Chen',
+        specialist: 'Pediatrician',
+        linkedinLink: '',
+        fbLink: '',
+        instaLink: '',
+      },
+    ],
+  },
+  {
+    doctorGroup: [
+      {
+        id: 4,
+        image: doctor1,
+        name: 'Dr. Marcus Davis',
+        specialist: 'Oncologist',
+        linkedinLink: '',
+        fbLink: '',
+        instaLink: '',
+      },
+      {
+        id: 5,
+        image: doctor2,
+        name: 'Dr. Maya Patel',
+        specialist: 'Gynecologist',
+        linkedinLink: '',
+        fbLink: '',
+        instaLink: '',
+      },
+      {
+        id: 6,
+        image: doctor3,
+        name: 'Dr. Benjamin Foster',
+        specialist: 'Orthopedic Surgeon',
+        linkedinLink: '',
+        fbLink: '',
+        instaLink: '',
+      },
+    ],
+  },
+  {
+    doctorGroup: [
+      {
+        id: 7,
+        image: doctor1,
+        name: 'Dr. Elena Rodriguez',
+        specialist: 'Neurology',
+        linkedinLink: '',
+        fbLink: '',
+        instaLink: '',
+      },
+      {
+        id: 8,
+        image: doctor2,
+        name: 'Dr. Alexander Barnes',
+        specialist: 'Dermatologist',
+        linkedinLink: '',
+        fbLink: '',
+        instaLink: '',
+      },
+      {
+        id: 9,
+        image: doctor3,
+        name: 'Dr. Rachel Carter',
+        specialist: 'Biologist',
+        linkedinLink: '',
+        fbLink: '',
+        instaLink: '',
+      },
+    ],
+  },
+];
