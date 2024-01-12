9071ba950309e1cdfaa34921a78c9daa5d8332ca
IanPrinceD <princedumanhug@gmail.com>
2024-01-12 19:47:43 +0800

    #112 : set up responsive dashsidebar

--- src/DashboardLayout/index.jsx
+++ src/DashboardLayout/index.jsx
@@ -2,15 +2,26 @@ import { Box, Container, Stack } from '@mui/material'
 import { Outlet } from 'react-router-dom'
 import DashFooter from '../components/DashFooter'
 import DashHeader from '../components/DashHeader'
+import DashMenuIcon from '../components/DashMenuIcon'
 import DashSidebar from '../components/DashSidebar'
 
 function DashboardLayout() {
   return (
     <Stack direction="row" sx={{ height: '100vh' }}>
-      <Box sx={{ flex: 1, background: '#FFF' }}>
+      <Box
+      // sx={{ flex: 1, background: '#FFF' }}
+      >
+        <DashMenuIcon />
         <DashSidebar />
       </Box>
-      <Box sx={{ flex: 5, background: 'teal' }}>
+      <Box
+        sx={{
+          //   flex: 5,
+          background: 'teal',
+          width: '100%',
+          //   '@media (max-width: 1024px)': { width: '100%', flex: '1' },
+        }}
+      >
         <DashHeader />
         <Box component="main">
           <Container>
--- src/components/DashSidebar/index.jsx
+++ src/components/DashSidebar/index.jsx
@@ -9,64 +9,68 @@ import {
   Typography,
 } from '@mui/material'
 import { Link, useLocation } from 'react-router-dom'
-import { dashBoardButton } from './data'
 import { Logo, RoleContainer } from './DashSidebar.Element'
+import { dashBoardButton } from './data'
 
 function DashSidebar() {
   const location = useLocation()
 
   return (
     <>
-      <Box
-        component="section"
-        sx={{ display: 'flex', flexDirection: 'column' }}
-      >
-        <Box>
-          <Logo variant="h1">
-            Al-
-            <Box component="span" sx={{ color: '#159EEC' }}>
-              Ameer
-            </Box>
-          </Logo>
-          <RoleContainer>
-            <Typography
-              sx={{ textAlign: 'center', fontSize: '2rem', fontWeight: '600' }}
-            >
-              Administrator
-            </Typography>
-            <Typography
-              sx={{
-                textAlign: 'center',
-                fontSize: '1.5rem',
-                fontWeight: '300',
-              }}
-            >
-              admin@al-ameer.com
-            </Typography>
-          </RoleContainer>
+        <Box
+          component="section"
+          sx={{ display: 'flex', flexDirection: 'column', width: '30rem' }}
+        >
+          <Box>
+            <Logo variant="h1">
+              Al-
+              <Box component="span" sx={{ color: '#159EEC' }}>
+                Ameer
+              </Box>
+            </Logo>
+            <RoleContainer>
+              <Typography
+                sx={{
+                  textAlign: 'center',
+                  fontSize: '2rem',
+                  fontWeight: '600',
+                }}
+              >
+                Administrator
+              </Typography>
+              <Typography
+                sx={{
+                  textAlign: 'center',
+                  fontSize: '1.5rem',
+                  fontWeight: '300',
+                }}
+              >
+                admin@al-ameer.com
+              </Typography>
+            </RoleContainer>
+          </Box>
+          <Divider />
+          <List sx={{ margin: '0 auto', color: '#159EEC' }}>
+            {dashBoardButton.map(({ id, path, label, icon }) => (
+              <Link
+                key={id}
+                to={path}
+                style={{
+                  color: location.pathname === path ? '#1F2B6C' : '#159EEC',
+                }}
+              >
+                <ListItem>
+                  <ListItemButton>
+                    <ListItemIcon sx={{ marginRight: '-.5rem' }}>
+                      {icon}
+                    </ListItemIcon>
+                    <ListItemText primary={label} />
+                  </ListItemButton>
+                </ListItem>
+              </Link>
+            ))}
+          </List>
         </Box>
-        <Divider />
-        <List sx={{ margin: '0 auto', color: '#159EEC' }}>
-          {dashBoardButton.map(({ id, path, label, icon }) => (
-            <Link
-              key={id}
-              to={path}
-              style={{
-                color: location.pathname === path ? '#1F2B6C' : '#159EEC',
-              }}
-            >
-              <ListItem>
-                <ListItemButton>
-                  <ListItemIcon sx={{ marginRight: '-.5rem' }}>
-                    {icon}
-                  </ListItemIcon>
-                  <ListItemText primary={label} />
-                </ListItemButton>
-              </ListItem>
-            </Link>
-          ))}
-        </List>
-      </Box>
     </>
   )
 }
